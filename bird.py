19ff9ea64a74971d48d077c20e85a9b33b7b3024
Valerij Medviď <vmedvid@gmail.com>
2020-12-19 16:08:14 +0100

    add flapwings

--- bird.py
+++ bird.py
@@ -52,6 +52,12 @@ def rotate_bird(bird):
     return new_bird
 
 
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect = new_bird.get_rect(center=(100, bird_rect.centery))
+    return new_bird, new_bird_rect
+
+
 if __name__ == "__main__":
     pygame.init()
 
@@ -66,9 +72,26 @@ if __name__ == "__main__":
     floor_surface = pygame.transform.scale2x(floor_surface)
     floor_x_pos = 0
 
-    bird_surface = pygame.image.load("assets/yellowbird-midflap.png").convert_alpha()
-    bird_surface = pygame.transform.scale2x(bird_surface)
+    bird_downflap = pygame.transform.scale2x(
+        pygame.image.load("assets/yellowbird-downflap.png").convert_alpha()
+    )
+    bird_midflap = pygame.transform.scale2x(
+        pygame.image.load("assets/yellowbird-midflap.png").convert_alpha()
+    )
+    bird_upflap = pygame.transform.scale2x(
+        pygame.image.load("assets/yellowbird-upflap.png").convert_alpha()
+    )
+    bird_frames = [bird_downflap, bird_midflap, bird_upflap]
+    bird_index = 0
+    bird_surface = bird_frames[bird_index]
     bird_rect = bird_surface.get_rect(center=(100, 512))
+
+    BIRDFLAP = pygame.USEREVENT + 1
+    pygame.time.set_timer(BIRDFLAP, 200)
+
+    # bird_surface = pygame.image.load("assets/yellowbird-midflap.png").convert_alpha()
+    # bird_surface = pygame.transform.scale2x(bird_surface)
+    # bird_rect = bird_surface.get_rect(center=(100, 512))
 
     pipe_surface = pygame.image.load("assets/pipe-green.png").convert()
     pipe_surface = pygame.transform.scale2x(pipe_surface)
@@ -94,6 +117,14 @@ if __name__ == "__main__":
 
             if event.type == SPAWNPIPE:
                 pipe_list.extend(create_pipe())
+
+            if event.type == BIRDFLAP:
+                if bird_index < 2:
+                    bird_index += 1
+                else:
+                    bird_index = 0
+
+                bird_surface, bird_rect = bird_animation()
 
         screen.blit(bg_surface, (0, 0))
 
