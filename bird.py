b7aef3d2e287534423821efa6ec6ed86d81c1d0d
Valerij Medviď <vmedvid@gmail.com>
2020-12-19 14:42:43 +0100

    basic moving background

---
+++ bird.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 900))
+    screen.blit(floor_surface, (floor_x_pos + 576, 900))
+
+
+pygame.init()
+pygame.display.set_caption("Flappy Bird Py")
+screen = pygame.display.set_mode((576, 1024))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load("assets/background-day.png").convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load("assets/base.png").convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(bg_surface, (0, 0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+    pygame.display.update()
+    clock.tick(120)
